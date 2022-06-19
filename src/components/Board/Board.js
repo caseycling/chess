4da95d62542889ebc7ff005729197c950e9f9e94
Steve Geluso <stevegeluso@gmail.com>
2022-06-19 00:55:17 -0400

    displaying moves as messsage

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Board.css';
 
+import { useState } from 'react';
+
 const column = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const row = [1, 2, 3, 4, 5, 6, 7, 8];
 
+const MoveMessage = ({clicks}) => {
+  if (clicks.length === 1) {
+    return <div className="move-message">{clicks[0]}</div>
+  } else if (clicks.length === 2) {
+    return <div className="move-message">{clicks[0]} to {clicks[1]}</div>
+  }
+};
+
 const Board = () => {
+  let [clickState, setClickState] = useState([]);
+
   let board = [];
 
   for (let i = 7; i >= 0; i--) {
@@ -13,16 +25,29 @@ const Board = () => {
     }
   }
 
+  function handleClick(rowCol) {
+    // handle pairs of clicks like moving e2 to e4 is stored as [e2, e4]
+    // once two clicks are registered clicking another time resets the array for the next pair of clicks
+    if (clickState.length < 2) {
+      setClickState([...clickState, rowCol]);
+    } else {
+      setClickState([rowCol]);
+    }
+  }
+
   return (
-    <div className='board-cont'>
-      {board.map((sqr) => {
-        return (
-          <div className='board-sqr' id={sqr}>
-            {' '}
-            {sqr}{' '}
-          </div>
-        );
-      })}
+    <div>
+      <div className='board-cont'>
+        {board.map((sqr) => {
+          return (
+            <div className='board-sqr' key={sqr} id={sqr} onClick={() => handleClick(sqr)}>
+              {' '}
+              {sqr}{' '}
+            </div>
+          );
+        })}
+      </div>
+      <MoveMessage clicks={clickState} />
     </div>
   );
 };
