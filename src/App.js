afb9d7fb710312b120473c00e05b946a1defd57d
Casey <caseyclinga@gmail.com>
2022-09-29 11:35:17 -0700

    Added logic to track if piece has moved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Board from './components/Board/Board';
 import LoginPage from './pages/LoginPage';
 import './App.css';
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 
 import { Google } from 'react-oauth2';
 
